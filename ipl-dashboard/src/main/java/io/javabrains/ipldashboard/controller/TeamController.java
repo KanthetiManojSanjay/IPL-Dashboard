10475efe388dfccad65ba31edde669d911d251a8
kansanja <sanjay.kantheti@e.lloydsbanking.com>
2021-08-10 20:45:03 +0530

    React App with Team Page

--- ipl-dashboard/src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -1,19 +1,15 @@
 package io.javabrains.ipldashboard.controller;
 
-import java.util.List;
-
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.javabrains.ipldashboard.model.Match;
 import io.javabrains.ipldashboard.model.Team;
 import io.javabrains.ipldashboard.repository.MatchRepository;
 import io.javabrains.ipldashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
