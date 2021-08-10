c858751ac7c861eb2c97ecb1b1d63026cb04292a
kansanja <sanjay.kantheti@e.lloydsbanking.com>
2021-08-10 21:30:09 +0530

    Completed UI elements for Team Page

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,27 +1,32 @@
 import { React, useEffect, useState } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
 
 export const TeamPage = () => {
-  const [team, setTeam] = useState({matches:[]});
+  const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
 
   useEffect(() => {
     const fetchMathes = async () => {
-      const response = await fetch(
-        "http://localhost:8080/team/Delhi%20Capitals"
-      );
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
       const data = await response.json();
       console.log(data);
       setTeam(data);
     };
     fetchMathes();
-  }, []);
+  }, [teamName]);
 
+  if (!team || !team.teamName) {
+    return <h1>team not found</h1>;
+  }
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]} />
-      {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName={team.teamName} match={match} />
+      ))}
     </div>
   );
 };
