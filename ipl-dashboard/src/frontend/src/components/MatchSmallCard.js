9d2dd6360cfad120cd1b4541298f7dc0f655e85e
kansanja <sanjay.kantheti@e.lloydsbanking.com>
2021-08-11 13:11:44 +0530

    Styled the team page

--- ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
+++ ipl-dashboard/src/frontend/src/components/MatchSmallCard.js
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 export const MatchSmallCard = ({ teamName, match }) => {
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div className="MatchSmallCard">
-      <h3>
-        vs
+    <div className={isMatchWon ? "MatchSmallCard won-card" : "MatchSmallCard lost-card"}>
+      <span className="vs">vs</span>
+      <h1>
         <Link to={otherTeamRoute}>{otherTeam}</Link>
-      </h3>
-      <p>
+      </h1>
+      <p className="match-result">
         {match.matchWinner} won by {match.resultMargin} {match.result}
       </p>
     </div>
